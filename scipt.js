cebe4d535683473c1f80285f8387b401ed6fb1eb
Ash-awa <enlisted485@gmail.com>
2025-02-02 22:04:03 +0800

    Update scipt.js

--- scipt.js
+++ scipt.js
@@ -7,7 +7,7 @@ function selectOption(option) {
         // Flash rainbow colors
         flashRainbowColors(function() {
             document.getElementById('question').style.display = 'none'; // Hide the question
-            displayCatHeart(); // Display the cat-heart.gif
+            displayCat(); // Display the cat-heart.gif
         });
     } else if (option === 'no') {
         // Change text on the "No" button to "You sure?"
@@ -49,7 +49,7 @@ function displayCat() {
     // Set the source (file path) for the cat image
     catImage.src = 'cat.gif'; // Assuming the cat image is named "cat.gif"
     // Set alternative text for the image (for accessibility)
-    catImage.alt = 'cat';
+    catImage.alt = 'Cat';
     // When the cat image is fully loaded, add it to the image container
     catImage.onload = function() {
         imageContainer.appendChild(catImage);
@@ -57,7 +57,7 @@ function displayCat() {
 }
 
 // Function to display the cat-heart.gif
-function displayCatHeart() {
+function displayCat() {
     // Clear existing content in the image container
     document.getElementById('image-container').innerHTML = '';
     // Get the container where the image will be displayed
